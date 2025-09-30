35aa463fb71930b11e53d9cf44a11b13f97173d5
luckytzu <tatindylan@gmail.com>
2025-09-30 21:47:24 +0200

    feat(home): add logout button with Firebase sign-out

--- app/src/main/java/fr/upjv/lesombresduson/Home.java
+++ app/src/main/java/fr/upjv/lesombresduson/Home.java
@@ -1,13 +1,33 @@
 package fr.upjv.lesombresduson;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class Home extends AppCompatActivity {
+
+    private Button btnLogout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activty_home);
+        setContentView(R.layout.activity_home);
+
+        btnLogout = findViewById(R.id.button_logout);
+
+        btnLogout.setOnClickListener(v -> {
+            // Déconnexion Firebase
+            FirebaseAuth.getInstance().signOut();
+
+            // Redirection vers la page de login
+            Intent intent = new Intent(Home.this, MainActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK); // supprime l'historique
+            startActivity(intent);
+            finish();
+        });
     }
 }
